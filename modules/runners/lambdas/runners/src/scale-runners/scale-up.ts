80d6e2cce1f1b78f15cc827602d13de72cda2872
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2021-12-09 14:57:36 -0800

    Revert "Revert "Revert "simplify scale-down, remove factories"""
    This reverts commit 5cd5ad6d6e72b63ee18679019756b2a4aa101dcf.

--- modules/runners/lambdas/runners/src/scale-runners/scale-up.ts
+++ modules/runners/lambdas/runners/src/scale-runners/scale-up.ts
@@ -1,4 +1,10 @@
-import { listRunners, createRunner, RunnerType, createGitHubClientForRunner, listGithubRunners } from './runners';
+import {
+  listRunners,
+  createRunner,
+  RunnerType,
+  createGitHubClientForRunnerFactory,
+  listGithubRunnersFactory,
+} from './runners';
 import { createOctoClient, createGithubAuth } from './gh-auth';
 import LRU from 'lru-cache';
 import YAML from 'yaml';
@@ -129,6 +135,9 @@ async function allRunnersBusy(
   repo: string,
   enableOrgLevel: boolean,
 ): Promise<boolean> {
+  const createGitHubClientForRunner = createGitHubClientForRunnerFactory();
+  const listGithubRunners = listGithubRunnersFactory();
+
   const githubAppClient = await createGitHubClientForRunner(org, repo, enableOrgLevel);
   const ghRunners = await listGithubRunners(githubAppClient, org, repo, enableOrgLevel);
 
@@ -157,6 +166,8 @@ async function GetRunnerTypes(org: string, repo: string, enableOrgLevel: boolean
     return runnerTypeCache.get(runnerTypeKey) as Map<string, RunnerType>;
   }
   console.debug(`[GetRunnerTypes] Grabbing runnerTypes`);
+
+  const createGitHubClientForRunner = createGitHubClientForRunnerFactory();
 
   const githubAppClient = await createGitHubClientForRunner(org, repo, enableOrgLevel);
 
